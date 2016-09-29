923ea01b5116163e0bd188e39dae076c22d52ba0
Mary Zelenska <marina.zelenskaya@gmail.com>
2016-09-29 17:07:06 +0300

    Environment configuration added.

--- ProjectKeeperSwift/ProjectKeeperSwift/Globals/Environment/Environment.swift
+++ ProjectKeeperSwift/ProjectKeeperSwift/Globals/Environment/Environment.swift
@@ -13,16 +13,33 @@ class Environment: NSObject {
     // MARK: Properties
     
     static let sharedInstance = Environment ()
-    let projectsUrl = "http://91.250.82.77:8081/3ssdemo/prj/json/projects.php"
-    let relatedAssetsUrl = "http://91.250.82.77:8081/3ssdemo/prj/json/galleryAssets.php?projectId={ID}"
-    let clientsUrl = "http://91.250.82.77:8081/3ssdemo/prj/json/clients.php"
+    var projectsUrl: String {
+        get {
+            return (environmentDictionary["apiProjectsList"] as? String)!
+        }
+    }
+    var clientsUrl: String {
+        get {
+            return (environmentDictionary["apiClients"] as? String)!
+        }
+    }
+    var relatedAssetsUrl: String {
+        get {
+            return (environmentDictionary["apiProjectAssets"] as? String)!
+        }
+    }
+    var environmentDictionary: [String : AnyObject]
     
     
     
     // MARK: Initializers
     
     private override init() {
-        
+        let bundle = NSBundle.mainBundle()
+        let plistPath = bundle.pathForResource("Environments", ofType: "plist")
+        let environmentsDict = NSDictionary.init(contentsOfFile: plistPath!)
+        environmentDictionary = environmentsDict as! [String : AnyObject]
     }
+    
     
 }
